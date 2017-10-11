6877f9e95d3e0ad85e0433840acf5ca8e74a6fc5
Justin Balogh <justin_balogh@hotmail.com>
2017-10-10 21:16:58 -0400

    first

--- MathTutorProject/MathTutorProject.cpp
+++ MathTutorProject/MathTutorProject.cpp
@@ -2,10 +2,43 @@
 //
 
 #include "stdafx.h"
-
-
-int main()
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void main()
 {
-    return 0;
-}
 
+	int firstNumer;
+	int secondNumer;
+	int firstDenom;
+	int secondDenom;
+	double firstFraction;
+	double secondFraction;
+	cout << setprecision(2) << fixed << showpoint; // output flags
+
+	cout << "Enter the first numberator: ";
+	cin >> firstNumer; cout << endl;
+	cout << "Enter the first denominator: ";
+	cin >> firstDenom; cout << endl;
+	cout << endl << endl;
+	cout << "Enter the second numberator: ";
+	cin >> secondNumer; cout << endl;
+	cout << "Enter the second denominator: ";
+	cin >> secondDenom; cout << endl;
+	cout << endl;
+
+	int multiplicationResultOne = firstNumer * secondDenom;
+	int multiplicationResultTwo = firstDenom * secondNumer;
+	// for displaying the multiplication work
+
+	firstFraction = (double)firstNumer / (double)firstDenom;
+	secondFraction = (double)secondNumer / (double)secondDenom;
+
+	cout << "Step 1. Original equation: " << firstNumer << "/" << firstDenom << " / " << secondNumer << "/" << secondDenom << " = ?" << endl;
+	// add a pause here
+	cout << "Step 2. Convert equation: " << firstNumer << "/" << firstDenom << " * " << secondDenom << "/" << secondNumer << " = ?" << endl;
+	// pause
+	cout << "Step 3. Perform Multiplication: " << firstNumer << "/" << firstDenom << " * " << secondDenom << "/" << secondNumer << " = " << multiplicationResultOne << "/" << multiplicationResultTwo << endl;
+	// pause
+	cout << "Convert to decimal: " << firstNumer;
+}
